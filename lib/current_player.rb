1e451611d52814a20419530663268a90803d0a19
Jae Yong Kim <kim900180@gmail.com>
2018-07-21 04:22:39 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,11 +2,10 @@ def turn_count(board)
   counter = 0
   turns = 0
   board.each do |turn|
-    if board[counter] == "X" || board[counter] == "O"
+    if turn == "X" || turn == "O"
       counter += 1
       turns +=1
     else
-      counter +=1
       turns
     end
   end
